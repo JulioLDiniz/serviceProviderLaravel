19cd85db3d4ab883b9f542e831d104fe4f075c06
Júlio Liarte Diniz <academicojulio@gmail.com>
2018-12-31 15:59:37 -0200

    Finished all modules

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\User;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->singleton('user', function(){
+            return User::find(1);
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
